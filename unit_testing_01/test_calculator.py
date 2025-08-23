7516bfa5f70ec3788a398b4d9525e960b5d3f685
Valeriia Nedashkivska <nedashkivskavaleriia@gmail.com>
2025-08-23 23:03:54 +0300

    Create unit_testing_01 package

--- unit_testing_01/test_calculator.py
+++ unit_testing_01/test_calculator.py
@@ -10,3 +10,13 @@ class TestsCalculator(unittest.TestCase):
     def test_add_functionality_two_negative_number(self):
         result = calcucator.calc_add(-10, -20)
         self.assertEqual(result, -30)
+
+
+class TestsCalculator2(unittest.TestCase):
+    def test_add_functionality2(self):
+        result = calcucator.calc_add(10, 20)
+        self.assertEqual(result, 30)
+
+    def test_add_functionality_two_negative_number2(self):
+        result = calcucator.calc_add(-10, -20)
+        self.assertEqual(result, -30)
